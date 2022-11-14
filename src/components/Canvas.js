84f546e92c524ea421c233471c92f788c8fb134e
aviehu <aviehu@gmail.com>
2022-11-14 15:38:18 +0200

    refactor

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,42 +1,84 @@
-import {useRef, useEffect} from "react";
+import {useRef, useEffect, useState} from "react";
 
-export default function Canvas({positions}) {
+export default function Canvas({positions, clearDrawing, setClearDrawing}) {
     const canvasRef = useRef(null)
     const POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
         ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
         ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
         ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
         ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
     const widthMult = 640 / 1920
     const heightMult = 480 / 1080
 
     useEffect(() => {
+        if(clearDrawing) {
+            setTimeout(() => {
+                const canvas = canvasRef.current
+                const ctx = canvas.getContext('2d')
+                ctx.clearRect(0,0, canvas.width, canvas.height)
+                setClearDrawing(false)
+            }, 3000)
+        }
+    }, [clearDrawing])
+
+    function drawCircles(ctx, positions) {
+        return new Promise((resolve) => {
+            for(const [key, value] of Object.entries(positions)) {
+                if(positions[key]) {
+                    ctx.fillStyle = key === "LElbow" || key === "LShoulder" ? "blue" : key === "RElbow" || key === "RShoulder" ? "yellow" : "red";
+                    ctx.beginPath();
+                    ctx.arc(value[0] * widthMult, value[1] * heightMult, 3, 0, 2 * Math.PI);
+                    ctx.fill();
+                }
+            }
+            resolve()
+        })
+    }
+
+    function drawLines(ctx, positions) {
+        return new Promise((resolve) => {
+            for(const pair of POSE_PAIRS) {
+                const from = pair[0]
+                const to = pair[1]
+                const fromPoint = positions[from]
+                const toPoint = positions[to]
+                if(fromPoint && toPoint) {
+                    ctx.strokeStyle = "blue";
+                    ctx.beginPath()
+                    ctx.moveTo(fromPoint[0] * widthMult, fromPoint[1] * heightMult)
+                    ctx.lineTo(toPoint[0] * widthMult, toPoint[1] * heightMult)
+                    ctx.stroke()
+                }
+            }
+            resolve()
+        })
+    }
+
+    async function draw(positions) {
+        const currPos = {...positions}
+        if(currPos["LShoulder"] && currPos["RShoulder"] && currPos["LShoulder"][0] < currPos["RShoulder"][0]) {
+            const temp = currPos["LShoulder"]
+            currPos["LShoulder"] = currPos["RShoulder"]
+            currPos["RShoulder"] = temp
+        }
+        if(currPos["LElbow"] && currPos["RElbow"] && currPos["LElbow"][0] < currPos["RElbow"][0]) {
+            const temp = currPos["LElbow"]
+            currPos["LElbow"] = currPos["RElbow"]
+            currPos["RElbow"] = temp
+        }
         const canvas = canvasRef.current
         const ctx = canvas.getContext('2d')
         ctx.clearRect(0,0, canvas.width, canvas.height)
-        for(const [key, value] of Object.entries(positions)) {
-            if(positions[key]) {
-                ctx.beginPath();
-                ctx.arc(value[0] * widthMult, value[1] * heightMult, 5, 0, 2 * Math.PI);
-                ctx.stroke();
-            }
-        }
-        for(const pair of POSE_PAIRS) {
-            const from = pair[0]
-            const to = pair[1]
-            const fromPoint = positions[from]
-            const toPoint = positions[to]
-            if(fromPoint && toPoint) {
-                console.log(pair)
-                ctx.beginPath()
-                ctx.moveTo(fromPoint[0] * widthMult, fromPoint[1] * heightMult)
-                ctx.lineTo(toPoint[0] * widthMult, toPoint[1] * heightMult)
-                ctx.stroke()
-            }
-        }
+        await drawCircles(ctx, currPos)
+        await drawLines(ctx, currPos)
+    }
+
+    useEffect( () => {
+        draw(positions)
     }, [positions])
 
     return (
-        <canvas width={640} height={480} ref={canvasRef} style={{zIndex:5, position: "absolute", left:0, top:0}}/>
+        <canvas width={"640px"} height={"480px"} ref={canvasRef} style={{zIndex:5, position: "absolute", left:0, top:0}}/>
     )
 }
--- src/components/WebCam.js
+++ src/components/WebCam.js
@@ -1,33 +1,48 @@
 import Webcam from "react-webcam";
 import Canvas from "./Canvas";
-import {useEffect, useState} from "react";
+import {useState, useRef} from "react";
+import {Button} from "@mui/material";
 
 export default function WebCam() {
     const [positions, setPositions] = useState({})
+    const [isRunning, setIsRunning] = useState(false)
+    const [runningInterval, setRunningInterval] = useState(null)
+    const [clearDrawing, setClearDrawing] = useState(false)
 
-    async function onClickHandler(getScreenshot) {
-        const imageSrc = getScreenshot({width: 1920, height: 1080});
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({image: imageSrc})
-        };
-        const response = await fetch(`http://localhost:3001/image`, requestOptions)
-        const ans = JSON.parse(await response.json())
-        setPositions(ans)
+    async function startDrawing(getScreenshot) {
+        setIsRunning(true)
+        const interval = setInterval(async () => {
+            const imageSrc = getScreenshot({width: 1920, height: 1080});
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({image: imageSrc})
+            };
+            const response = await fetch(`http://localhost:3001/image`, requestOptions)
+            const ans = JSON.parse(await response.json())
+            setPositions(ans)
+        }, 750)
+        setRunningInterval(interval)
+    }
+
+    function stopDrawing() {
+        setIsRunning(false)
+        setClearDrawing(true)
+        clearInterval(runningInterval)
+        setRunningInterval(null)
     }
 
     return (
         <div style={{position:"relative"}}>
-            <Webcam style={{zIndex:0, width:"640", height:"480", position:"absolute", left:0, top:0}} screenshotQuality={0.5} mirrored={true} videoConstraints={{facingMode: "user"}} screenshotFormat="image/jpeg">
+            <Webcam style={{zIndex:0, width:"640px", height:"480px", position:"absolute", left:0, top:0}} screenshotQuality={0.4} mirrored={true} videoConstraints={{facingMode: "user", width: 640, height: 480,}} screenshotFormat="image/jpeg">
                 {({ getScreenshot }) => (
-                    <button style={{position:"absolute", zIndex:10}} onClick={() => onClickHandler(getScreenshot)}>
+                    <button disabled={isRunning} style={{position:"absolute", zIndex:10}} onClick={() => startDrawing(getScreenshot)}>
                         Capture photo
                     </button>
                 )}
             </Webcam>
-            <Canvas positions={positions}></Canvas>
+            <Canvas positions={positions} clearDrawing={clearDrawing} setClearDrawing={setClearDrawing}></Canvas>
+            <Button style={{position: "absolute", right:0, top: 0}} color="primary" onClick={stopDrawing}>Stop</Button>
         </div>
-
     )
 }
