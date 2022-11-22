9cad2d3c0a2296c01896570980983ceff8b0c10d
aviehu <aviehu@gmail.com>
2022-11-22 12:10:48 +0200

    .env

---
+++ src/util/envVars.js
@@ -0,0 +1,7 @@
+export const screenshotWidth = parseInt(process.env.REACT_APP_SCREENSHOT_WIDTH)
+export const screenshotHeight = parseInt(process.env.REACT_APP_SCREENSHOT_HEIGHT)
+export const screenshotQuality = Number(process.env.REACT_APP_SCREENSHOT_QUALITY)
+export const serverURL = process.env.REACT_APP_SERVER_URL
+export const videoWidth = parseInt(process.env.REACT_APP_VIDEO_WIDTH)
+export const videoHeight = parseInt(process.env.REACT_APP_VIDEO_HEIGHT)
+export const sampleInterval = parseInt(process.env.REACT_APP_SAMPLE_INTERVAL)
