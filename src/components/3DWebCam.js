51f58b9b1177590f42a6e1b473ba670da0e77f91
aviehu <aviehu@gmail.com>
2022-12-25 18:28:35 +0200

    webSocket

--- src/components/3DWebCam.js
+++ src/components/3DWebCam.js
@@ -3,6 +3,7 @@ import {useRef, useState, useEffect} from "react";
 import {Button} from "@mui/material";
 import { scoreThreshold, sampledVideoWidth } from '../util/envVars'
 import * as poseDetection from '@tensorflow-models/pose-detection'
+import useWebsocket from '../useWebsocket'
 import '@tensorflow/tfjs-backend-webgl';
 import drawCircles from "../util/drawCircles";
 import drawLines from "../util/drawLines";
@@ -19,6 +20,7 @@ export default function WebCam() {
     const canvasRef = useRef(null)
     const webcamRef = useRef(null)
     const clientWebcamRef = useRef(null)
+    const [msg, sendMsg] = useWebsocket()
 
     async function sampleCameraSize() {
         const features = {
@@ -76,7 +78,7 @@ export default function WebCam() {
             const estimationConfig = {flipHorizontal: true};
             const timestamp = performance.now();
             const poses = await blazePoseModel.estimatePoses(video, estimationConfig, timestamp);
-            console.log(poses)
+
             if(poses[0]) {
                 const currentTimeFrame = Date.now()
                 if(currentTimeFrame - lastTimeFrame > 1000) {
@@ -93,6 +95,7 @@ export default function WebCam() {
                         z: pos.z
                     }
                 })
+                sendMsg(JSON.stringify(ans))
                 recording.push(ans)
                 clearCanvas(ctx, canvasRef.current.width, canvasRef.current.height)
                 drawLines(ctx, ans)
