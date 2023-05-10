6547a4af32e8ee1f47495a25c308620d98b312a4
hilaklein <63915907+hilaklein@users.noreply.github.com>
2023-05-10 20:32:10 +0300

    pose matching

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import PosesPage from "./LoginPage/PosesPage";
 import AdminPage from "./LoginPage/AdminPage";
 import UsersPage from "./LoginPage/UsersPage";
 import logo from './logo.jpeg';
+import AppBar from './components/AppBar'
+import Test from './components/Test'
 import {
     BrowserRouter as Router,
     Route,
@@ -14,17 +16,8 @@ import {
 function App() {
 
     return (
-        <div>
-            <div style={{ position: 'absolute' }}>
-                <img style={{
-                    borderRadius: 50,
-                    height: 100,
-                    width: 100,
-                    marginLeft: 20,
-                    marginTop: 20
-                }} src={logo} alt="logo" />
-            </div>
-
+            
+        <AppBar>
             <Router>
                 <Routes>
                     <Route path="/" Component={LoginPage} />
@@ -33,9 +26,10 @@ function App() {
                     <Route path="/poses" Component={PosesPage} />
                     <Route path="/admin" Component={AdminPage} />
                     <Route path="/users" Component={UsersPage} />
+                    <Route path="/test" Component={Test} />
                 </Routes>
             </Router>
-        </div>
+        </AppBar>
     );
 }
 
