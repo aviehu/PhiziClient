18dd0f7663619644e0fa218bf44d57ea77840f81
aviehu <aviehu@gmail.com>
2023-05-14 20:42:13 +0300

    Pose From Webcam

--- src/components/PoseFromWebcam.js
+++ src/components/PoseFromWebcam.js
@@ -3,13 +3,10 @@ import { sampledVideoWidth } from "../util/envVars";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as mpPose from "@mediapipe/pose";
 import Webcam from "react-webcam";
-import { Button } from "@mui/material";
+import {Button, Snackbar, Stack} from "@mui/material";
 import getCameraRatio from "../util/getCameraRatio";
-import {clearCanvas, drawLines, drawCircles, drawUserSkeleton} from '../util/canvas'
-import get2DPositions from "../util/get2DPositions";
-import { curUser } from "./LoginPage";
-import api from "../util/api";
-import PoseMatchingCanvas from "../components/PoseMatchingCanvas";
+import {clearCanvas, drawUserSkeleton} from '../util/canvas'
+import PoseMatchingCanvas from "./PoseMatchingCanvas";
 
 const detectorConfig = {
     runtime: 'mediapipe',
@@ -17,27 +14,14 @@ const detectorConfig = {
     solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}`
 };
 
-export default function AppPage() {
+export default function PoseFromWebcam({onSubmit}) {
     const [isRunning, setIsRunning] = useState(false)
     const [blazePoseModel, setBlazePoseModel] = useState(null)
     const [cameraRatio, setCameraRatio] = useState(-1)
-    const [trainingPoses, setTrainingPoses] = useState(null)
-    const [fps, setFps] = useState(0)
-
+    const [screenShot, setScreenShot] = useState(null)
     const canvasRef = useRef(null)
-    const singlePoseRef = useRef(null)
     const webcamRef = useRef(null)
     const clientWebcamRef = useRef(null)
-    const user = useContext(curUser)
-    console.log(user)
-
-    async function getTrainingPoses(){
-        const response = await api.getTrainingPoses()
-            if (!response.error) {
-                setTrainingPoses(response[0])
-                return
-            }
-    }
 
     useEffect(() => {
         if (!webcamRef.current || cameraRatio < 0) {
@@ -64,16 +48,18 @@ export default function AppPage() {
             return
         }
         let internallIsRunning = true
+        setScreenShot(null)
         async function draw() {
             const ctx = canvasRef.current.getContext('2d')
-            if (!internallIsRunning) {
-                clearCanvas(ctx, canvasRef.current.width, canvasRef.current.height)
-                setFps(0)
-                return
-            }
             const video = webcamRef.current.video;
             const estimationConfig = { flipHorizontal: false };
             const timestamp = performance.now();
+            if (!internallIsRunning) {
+                clearCanvas(ctx, canvasRef.current.width, canvasRef.current.height)
+                const poses = await blazePoseModel.estimatePoses(video, estimationConfig, timestamp);
+                setScreenShot(poses[0])
+                return
+            }
             const poses = await blazePoseModel.estimatePoses(video, estimationConfig, timestamp);
             drawUserSkeleton(ctx, poses[0], canvasRef)
             setTimeout(draw, 0)
@@ -84,9 +70,8 @@ export default function AppPage() {
         }
     }, [isRunning, webcamRef.current, blazePoseModel, canvasRef.current])
 
-    async function startDrawing() {
+    function startDrawing() {
         setIsRunning(true)
-        getTrainingPoses()
     }
 
     function stopDrawing() {
@@ -96,9 +81,9 @@ export default function AppPage() {
     return (
         <div style={{position: 'absolute', backgroundColor: 'white', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <Button variant={'contained'} disabled={isRunning} style={{ position: "absolute", zIndex: 10, left: 15, top: 15}} onClick={() => startDrawing()}>
-                 Draw Skeleton
+                Draw Skeleton
             </Button>
-            <Button variant={'contained'} style={{ position: "absolute", zIndex: 10, right: 15, top: 15}} color="primary" onClick={stopDrawing}>Stop</Button>
+            <Button disabled={!isRunning || screenShot} variant={'contained'} style={{ position: "absolute", zIndex: 10, right: 15, top: 15}} color="primary" onClick={stopDrawing}>Take Picture</Button>
             <Webcam
                 ref={webcamRef}
                 style={{ zIndex: -1, position: "absolute", left: 0, top: 0 }}
@@ -118,14 +103,20 @@ export default function AppPage() {
                 null
             }
             {clientWebcamRef.current ?
-                    <canvas
-                        width={`${sampledVideoWidth}px`}
-                        height={`${sampledVideoWidth / cameraRatio}px`}
-                        ref={canvasRef}
-                        style={{ zIndex: 5, width: 800, height: 800 / cameraRatio, marginLeft: -800}}
-                    />
+                <canvas
+                    width={`${sampledVideoWidth}px`}
+                    height={`${sampledVideoWidth / cameraRatio}px`}
+                    ref={canvasRef}
+                    style={{ zIndex: 5, width: 800, height: 800 / cameraRatio, marginLeft: -800}}
+                />
                 : null}
-            { clientWebcamRef.current ? <PoseMatchingCanvas cameraRatio={cameraRatio} targetPose={trainingPoses}/> : null }
+            { screenShot ? <PoseMatchingCanvas cameraRatio={cameraRatio} targetPose={screenShot}/> : null }
+            { screenShot ?
+                <Button variant={'contained'} style={{ position: 'absolute', right: 30, bottom: 80 }} onClick={onSubmit}>
+                    Accept
+                </Button> :
+                null
+            }
         </div>
     )
 }
