bfc080e1300fd591f9f1b2687f5d94312d766ec0
BatyaBialik <bialikb@post.bgu.ac.il>
2023-05-15 18:53:36 +0300

    revert color change

--- src/components/PoseMatchingCanvas.js
+++ src/components/PoseMatchingCanvas.js
@@ -1,13 +1,13 @@
-import {sampledVideoWidth} from "../util/envVars";
-import {useEffect, useRef} from "react";
-import { drawUserSkeleton} from "../util/canvas";
+import { sampledVideoWidth } from "../util/envVars";
+import { useEffect, useRef } from "react";
+import { drawUserSkeleton, drawWantedPoseSkeleton } from "../util/canvas";
 
-export default function PoseMatchingCanvas({cameraRatio, targetPose}) {
+export default function PoseMatchingCanvas({ cameraRatio, targetPose }) {
 
     const canvasRef = useRef()
     console.log(targetPose)
     useEffect(() => {
-        if(!canvasRef || !canvasRef.current || !targetPose) {
+        if (!canvasRef || !canvasRef.current || !targetPose) {
             return
         }
         const ctx = canvasRef.current.getContext('2d')
@@ -15,6 +15,6 @@ export default function PoseMatchingCanvas({cameraRatio, targetPose}) {
     }, [canvasRef.current])
 
     return (
-        <canvas width={`${sampledVideoWidth}px`} height={`${sampledVideoWidth / cameraRatio}px`} ref={canvasRef} style={{ zIndex: 6, width: 800, height: 800 / cameraRatio, marginLeft: -800}} />
+        <canvas width={`${sampledVideoWidth}px`} height={`${sampledVideoWidth / cameraRatio}px`} ref={canvasRef} style={{ zIndex: 6, width: 800, height: 800 / cameraRatio, marginLeft: -800 }} />
     )
 }
--- src/util/canvas.js
+++ src/util/canvas.js
@@ -1,20 +1,20 @@
 import colorMap from "./colorMap";
-import {POSE_PAIRS} from "./pairs";
+import { POSE_PAIRS } from "./pairs";
 import get2DPositions from "./get2DPositions";
 
 export function clearCanvas(ctx, width, height) {
-    ctx.clearRect(0,0, width, height)
+    ctx.clearRect(0, 0, width, height)
 }
 
-export function drawLines(ctx, positions) {
+export function drawLines(ctx, positions, color) {
     console.log("drawing")
     POSE_PAIRS.forEach((pair) => {
         const from = pair[0]
         const to = pair[1]
         const fromPoint = positions.find((pos) => pos.part === from)
         const toPoint = positions.find((pos) => pos.part === to)
-        if(fromPoint && toPoint) {
-            ctx.strokeStyle = "rgb(26,255,255)";
+        if (fromPoint && toPoint) {
+            ctx.strokeStyle = color;
             ctx.lineWidth = 2
             ctx.beginPath()
             ctx.moveTo(fromPoint.x, fromPoint.y)
@@ -37,7 +37,16 @@ export function drawUserSkeleton(ctx, positions, canvasRef) {
     if (positions) {
         const poses2D = get2DPositions(positions)
         clearCanvas(ctx, canvasRef.current.width, canvasRef.current.height)
-        drawLines(ctx, poses2D)
+        drawLines(ctx, poses2D, "rgb(26,255,255)")
+        drawCircles(ctx, poses2D)
+    }
+}
+
+export function drawWantedPoseSkeleton(ctx, positions, canvasRef) {
+    if (positions) {
+        const poses2D = get2DPositions(positions)
+        clearCanvas(ctx, canvasRef.current.width, canvasRef.current.height)
+        drawLines(ctx, poses2D, "rgb(255,0,127)")
         drawCircles(ctx, poses2D)
     }
 }
