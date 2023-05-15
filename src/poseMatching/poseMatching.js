e6552a6fc5369cf596ab069d94ead90bcdc262b6
BatyaBialik <bialikb@post.bgu.ac.il>
2023-05-15 13:51:16 +0300

    poseMatching tests

--- src/poseMatching/poseMatching.js
+++ src/poseMatching/poseMatching.js
@@ -1,11 +1,16 @@
-import {calcAngles} from '../util/calc'
+import { calcAngles } from '../util/calc'
 import get3DPositions from "../util/get3DPositions";
 
 export default function isMatching(wantedPose, userPose) {
     const wantedPoseAngles = calcAngles(get3DPositions(wantedPose))
     const userPoseAngles = calcAngles(get3DPositions(userPose))
     for (let i = 0; i < wantedPoseAngles.length; i++) {
-        if (Math.abs(wantedPoseAngles[i].angle - userPoseAngles[i].angle) > 15) {
+        const wantedAngle = wantedPoseAngles[i]
+        const userAngle = userPoseAngles[i]
+        if (wantedAngle != -1 && userAngle === -1) {
+            return false
+        }
+        if (Math.abs(wantedAngle.angle - userAngle.angle) > 15) {
             return false
         }
     }
