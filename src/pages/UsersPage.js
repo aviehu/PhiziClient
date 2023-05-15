608f4f067e5b481a22488bf9fe08944e7ab07e00
hilaklein <63915907+hilaklein@users.noreply.github.com>
2023-05-15 18:28:53 +0300

    almost finished

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,18 +1,10 @@
 import { useState, useEffect} from "react";
-import { Divider, ListItemText, List, ListItem,Stack, Paper, FormControl, OutlinedInput,MenuItem, Checkbox, InputLabel, Select, Avatar, ListItemAvatar, Button } from "@mui/material";
+import { Divider, ListItemText, List,ListItem,ListItemSecondaryAction,Stack, Paper, FormControl, OutlinedInput,MenuItem, Checkbox, InputLabel, Select, Avatar, ListItemAvatar, Button } from "@mui/material";
 import api from "../util/api";
 import userLogo from "../person.png";
 
 
-
-
-
-export default function UsersPage() {
-
-    const [usersList, setUsersList] = useState([])
-    const [goals, setGoals] = useState([]);
-    
-    const ITEM_HEIGHT = 48
+const ITEM_HEIGHT = 48
     const ITEM_PADDING_TOP = 8
 
     const goalsNames = ['Upper Body', 'Legs', 'Shoulders'];
@@ -27,111 +19,113 @@ export default function UsersPage() {
           },
         },
       }
+    //   const handleGoalChange = async (event,email) => {
+    //     const values = event.target.value
+        
+    //     return values
+    // }
 
-      const handleGoalChange = (event) => {
-        const values = event.target.value
-        console.log(values)
-        setGoals(values)
-       }
-
-    function DisplayUsers(usersList) {
-        return usersList.map((user) => {
-            return (
-                <div  style={{display: 'flex', position: "absolute", height: "100%", width: "100%", justifyContent: "center", alignItems: "center"}}>
-                    <Paper style={{padding:10, display: 'flex', justifyContent: "center", alignItems: "center"}}>
-                        <Stack style={{textAlign: "center"}} direction="column" spacing={3}>
-                            <Stack style={{textAlign: "center"}} direction="row" >
-                                <ListItem alignItems="center">
-                                    <ListItemAvatar>
-                                        <Avatar alt="Remy Sharp" src={userLogo} />
-                                    </ListItemAvatar>
-                                </ListItem>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary="Name: "
-                                        secondary = {user.name}/>
-                                </ListItem>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary="Age: "
-                                        secondary = {user.age}/>
-                                </ListItem>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary="Weight: "
-                                        secondary = {user.weight}/>
-                                </ListItem>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary="Height: "
-                                        secondary = {user.height}/>
-                                </ListItem>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary="BMI: "
-                                        secondary = {Number(user.bmi).toFixed(2)}/>
-                                </ListItem>
-                            </Stack>
+function DisplayUsers({usersList, handleGoals}) {
+        return (
+            <Stack direction="column" spacing={3} style={{ justifyContent: "center", alignItems: "center"}}>
+                {usersList? usersList.map((user) => { 
+                return(
+                <Paper key={user.name} spacing={3} direction="column" style={{position: "absolute",display: 'flex', justifyContent: "center", alignItems: "center"}}>
+                    <List direction = "column" style={{ justifyItems:"center"}}>
+                        <ListItem>
+                            <Stack direction="row" spacing={3}>
+                                <ListItemAvatar>
+                                    <Avatar alt="Remy Sharp" src={userLogo} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Name: "
+                                    secondary = {user.name}/>
+                                <ListItemText
+                                    primary="Age: "
+                                    secondary = {user.age}/>
+                                <ListItemText
+                                    primary="Weight: "
+                                    secondary = {user.weight}/>
+                                <ListItemText
+                                    primary="Height: "
+                                    secondary = {user.height}/>
+                                <ListItemText
+                                    primary="BMI: "
+                                    secondary = {Number(user.bmi).toFixed(2)}/>
+                                </Stack>
+                            </ListItem>
                             
-                            <Stack style={{alignItems: "center", justifyContent:"center"}} direction="row" >
-                                <FormControl sx={{ m: 1, width: 300 }}>
+                            <ListItem>
+                                <FormControl sx={{ m: 1, width: 300 }} >
                                                 <InputLabel id="demo-multiple-checkbox-label">Goals</InputLabel>
                                                 <Select
                                                 labelId="demo-multiple-checkbox-label"
                                                 id="demo-multiple-checkbox"
                                                 multiple
-                                                value={goals}
-                                                onChange={handleGoalChange}
+                                                value={user.goals}
+                                                onChange={(event) => handleGoals(event.target.value,user._id) }
                                                 input={<OutlinedInput label="Goals" />}
                                                 renderValue={(selected) => selected.join(', ')}
                                                 MenuProps={goalsProps}
                                                 >
+                                                console.log("mygoals!",goals)
                                                 {goalsNames.map((goal) => (
                                                     <MenuItem key={goal} value={goal}>
-                                                        <Checkbox checked={goals.indexOf(goal) > -1} />
+                                                        <Checkbox checked={user.goals.indexOf(goal) > -1} />
                                                     <ListItemText primary={goal} />
                                                     </MenuItem>
                                                 ))}
                                                 </Select>
                                 </FormControl>
-                                
-                            </Stack>
-                            <Button disabled = {!goals} onClick={handleGoals(user.email)}>Add Goals</Button>
-                        </Stack>
-                        <Divider variant="inset" component="li" />
-                    </Paper>
-                </div>
-            )
-        })
-    };
+                            </ListItem>
+                    </List>
+                </Paper> 
+                )}) : null}    
+            </Stack> 
+        )                                      
+};
+
+
+
+export default function UsersPage() {
+    const [usersList, setUsersList] = useState([])
+    // const [goals, setGoals] = useState([]);
+    
+    async function load() {
+        const response = await api.getAllUsers()
+        if (!response.error) {
+            setUsersList(response)
+            console.log(response)
+            return
+        }
+        alert(response.error)
+
+    }
+
+    
 
     useEffect(() => {
-        async function load() {
-            const response = await api.getAllUsers()
-            if (!response.error) {
-                setUsersList(response)
-                console.log(response)
-                return
-            }
-            alert(response.error)
-
-        }
         load()
-    }, [])
-
+    }, [])    
     
-    
-    function handleGoals(email) {
-        console.log("email, goals", [email,goals])
+    async function handleGoals(goals,id) {
         // await api.editUserGoals({email: email , goals: goals})
+        // const newgoals = event.target.value
+        const req = {id , goals}
+        const res = await api.editUserGoals(req)
+        if(!res.error){
+            console.log("response: ", res)
+            load()
+        }
     }
     
 
     return (
         <div style={{ display: "flex", position: "absolute", height: "100%", width: "100%", justifyContent: "center", alignItems: "center" }}>
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                {usersList ? DisplayUsers(usersList) : []}
-            </List>
+            {/* <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}> */}
+            
+                {usersList ? <DisplayUsers usersList={usersList} handleGoals={handleGoals} /> : []}
+            {/* </List> */}
         </div>
     )
 }
--- src/util/api.js
+++ src/util/api.js
@@ -19,7 +19,12 @@ const deleteRequestOptions = function () {
         method: 'DELETE'
     }
 };
-
+api.editUserGoals = async function(req){
+    console.log("req",req)
+    const requestOptions = postRequestOptions(req)
+    const response = await fetch(`${serverUrl}/editUserGoals`, requestOptions)
+    return await response.json()
+}
 api.login = async function (body) {
     const requestOptions = postRequestOptions(body)
     const response = await fetch(`${serverUrl}/login`, requestOptions)
